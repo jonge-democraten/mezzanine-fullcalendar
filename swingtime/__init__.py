f430ef848729e8cbf944b7c802cc740da0d51dbd
Lucas van Dijk <info@return1.net>
2015-01-11 22:28:03 +0100

    Add default app configuration

--- swingtime/__init__.py
+++ swingtime/__init__.py
@@ -1,5 +1,6 @@
 VERSION = (0, 4)
 
-#-------------------------------------------------------------------------------
 def get_version():
     return '.'.join([str(i) for i in VERSION])
+
+default_app_config = 'swingtime.apps.SwingtimeAppConfig'
---
+++ swingtime/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+class SwingtimeAppConfig(AppConfig):
+    name = "swingtime"
+    verbose_name = "events"
+    label = "Events"
