19f743d37ec62b08c8d5314f4691bed979edc9d5
Lucas van Dijk <info@return1.net>
2015-01-24 14:00:54 +0100

    Properly descend from DisplayableAdmin

--- swingtime/admin.py
+++ swingtime/admin.py
@@ -1,23 +1,40 @@
+from django.utils.translation import ugettext_lazy as _
 from django.contrib import admin
+from mezzanine.core.admin import TabularDynamicInlineAdmin, DisplayableAdmin
+
 from swingtime.models import *
 
 class EventCategoryAdmin(admin.ModelAdmin):
     list_display = ('name',)
 
-
-class OccurrenceInline(admin.TabularInline):
+class OccurrenceInline(TabularDynamicInlineAdmin):
     model = Occurrence
     extra = 1
 
-
-class EventAdmin(admin.ModelAdmin):
+class EventAdmin(DisplayableAdmin):
     list_display = ('title', 'event_category')
     list_filter = ('event_category',)
     search_fields = ('title', 'description', 'content', 'keywords')
-    exclude = ('slug', '_meta_title', 'gen_description', 'description')
+
+    fieldsets = (
+        (None, {
+            "fields": [
+                "title", "status", ("publish_date", "expiry_date"),
+                "event_category", "content"
+            ]
+        }),
+        (_("Meta data"), {
+            "fields": [
+                "_meta_title", "slug",
+                ("description", "gen_description"),
+                "keywords", "in_sitemap"
+            ],
+            "classes": ("collapse-closed",)
+        }),
+    )
 
     inlines = [OccurrenceInline]
 
-
 admin.site.register(Event, EventAdmin)
 admin.site.register(EventCategory, EventCategoryAdmin)
+
