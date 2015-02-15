d190e4466a28714bf0e04869dc11a940526031b1
Lucas van Dijk <info@return1.net>
2015-02-15 16:19:21 +0100

    Use the right way to limit queries

--- fullcalendar/templatetags/fullcalendar.py
+++ fullcalendar/templatetags/fullcalendar.py
@@ -9,7 +9,7 @@ def show_agenda(*args, **kwargs):
     qs = Occurrence.objects.upcoming()
 
     if 'limit' in kwargs:
-        qs.limit(int(kwargs['limit']))
+        qs = qs[:int(kwargs['limit'])]
 
     return {
         'occurrences': qs,
@@ -21,17 +21,16 @@ def get_agenda(*args, **kwargs):
     qs = Occurrence.site_related.upcoming()
 
     if 'limit' in kwargs:
-        qs.limit(int(kwargs['limit']))
+        return qs[:int(kwargs['limit'])]
 
     return qs
-
 
 @register.inclusion_tag('events/agenda_tag.html')
 def show_site_agenda(*args, **kwargs):
     qs = Occurrence.site_related.upcoming()
 
     if 'limit' in kwargs:
-        qs.limit(int(kwargs['limit']))
+        qs = qs[:int(kwargs['limit'])]
 
     return {
         'occurrences': qs
@@ -42,7 +41,7 @@ def get_site_agenda(*args, **kwargs):
     qs = Occurrence.site_related.upcoming()
 
     if 'limit' in kwargs:
-        qs.limit(int(kwargs['limit']))
+        return qs[:int(kwargs['limit'])]
 
     return qs
 
